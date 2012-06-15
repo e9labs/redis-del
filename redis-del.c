cfa3a95db170484b45c65d838670fbf4684c2e7f
Ben Weller <ben@kohort.com>
2012-06-15 18:06:07 -0400

    added redis-del.c

---
+++ redis-del.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "hiredis/hiredis.h"
+
+int main( int argc, char *argv[] ) {
+    redisReply *reply;
+    long int i;
+    char buffer[256];
+    char* keyString;
+    char* host;
+    int port;
+
+    if (argc < 2) {
+        printf("not enough options");
+    } else {
+
+        if (argc == 2) {
+            keyString = argv[1];
+            host = "127.0.0.1";
+            port = 6379;
+        } else if (argc == 3) {
+            keyString = argv[1];
+            host = argv[2];
+            port = 6379;
+
+        } else if (argc == 4) {
+            keyString = argv[1];
+            host = argv[2];
+            port = atoi(argv[3]);
+        }
+
+            redisContext *c = redisConnect(host, port);
+            if (c->err) {
+                printf("Error: %s\n", c->errstr);
+            }else{
+                printf("Connection Made! \n");
+            }
+
+            reply = redisCommand(c,  "KEYS %s", keyString);
+            if ( reply->type == REDIS_REPLY_ERROR )
+                printf( "Error: %s\n", reply->str );
+            else if ( reply->type != REDIS_REPLY_ARRAY )
+                printf( "Unexpected type: %d\n", reply->type );
+            else if (reply->elements > 0) {
+                printf("%zu matched keys. Delete them? [y/N]: ", reply->elements);
+                scanf ("%s", buffer);
+                printf("\n");
+                if (strcmp(buffer, "y") == 0 || strcmp(buffer, "Y") == 0) {
+                    for ( i=0; i<reply->elements; ++i ){
+                        redisCommand(c, "DEL %s", reply->element[i]->str);
+                        printf( "%lu: %s\n", i, reply->element[i]->str );
+                    }
+                    printf("\n%li objects removed\n", i);
+                } else {
+                    printf("\nCancelled!\n");
+                }
+            } else
+                printf("\nNo keys matched!\n");
+            freeReplyObject(reply);
+    }
+}
+
